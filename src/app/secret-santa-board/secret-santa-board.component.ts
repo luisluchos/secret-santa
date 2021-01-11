8c5707a2bd871ceb23673ea7748537d84fe0440e
Luis Marin <luismgval@gmail.com>
2021-01-11 23:34:18 +0100

    V1.2 need inprove shuffle function

--- src/app/secret-santa-board/secret-santa-board.component.ts
+++ src/app/secret-santa-board/secret-santa-board.component.ts
@@ -23,23 +23,66 @@ export class SecretSantaBoardComponent implements OnInit {
 
   friendsList: string[] = names; //lo traigo del archivo assets/json/names.ts
   secretSantaList: SecretSantaItemObj[]= [];
+  suffle_list:  string[];
+
+  newFriendlist: any = [ ]; //nueva lista con loas participantes correctos que no coinciden
+  newSuffleList: any = [ ]; //nueva lista con loas participantes correctos que no coinciden
 
   constructor() {
+    this.suffle() //al cargar la página realizamos el primer suffle
     this.crearSorteo()
+
+
   }
 
-  crearSorteo() {
-    var suffle_list = this.friendsList.slice().sort(function(){ //con Slice clonamos el array sin modificar el original
+
+suffle(){
+    this.suffle_list = this.friendsList.slice().sort(function(){ //con Slice clonamos el array sin modificar el original
       return Math.random()-0.5  //La función Math.random() nos devuelve un número aleatorio entre 0 y 0.9999..., lo que conseguimos al restarle 0.5 es que nos genere números negativos y positivos para que la función sort() nos re-ordene el array de forma aleatoria colocando un elemento delante otro detrás.
     });
+  }
 
-    this.friendsList.forEach((item, i) => {
-      var creacion_parejas = new Parejas (item, suffle_list[i]); //creamos el objeto, En function Parejas le hemos marcado la estructura, (le tenemos que pasar dos parametros para que moneten el name y el name_hidden)
+
+
+crearSorteo() {
+
+while (this.friendsList.length > 0) {
+
+  this.friendsList.forEach((item, i) => {
+// si los elementos de los 2 arrays son disitintos los vamos a meter en dos nuevos arrays, y los eliminaremos del array antiguo, "sacamos las papaleletas"
+    if (this.friendsList[i] !== this.suffle_list[i]){
+      let value = this.friendsList[i]; //el nombre del paricipante del 1º array
+      let value_hidden = this.suffle_list[i]; // el nombre del participante del 2º array
+// creamos los nuevos arrays con las parejas que son validas
+      this.newFriendlist.push(value)
+      this.newSuffleList.push(value_hidden)
+
+// Eliminamos las parejas del antiguo array, ya que luego haremos un suffle con los que se queden repetidos
+      this.friendsList= this.friendsList.filter(item=>item !== value)
+      this.suffle_list= this.suffle_list.filter(item=>item !== value_hidden)
+
+// creamos un array de objetos, que contendrán las parejas
+      var creacion_parejas = new Parejas (value, value_hidden); //creamos el objeto, En function Parejas le hemos marcado la estructura, (le tenemos que pasar dos parametros para que moneten el name y el name_hidden)
       this.secretSantaList.push(creacion_parejas); //con push lo metemos en el array de secretSanta, al final tendremos un array de objetos
-    })
-      console.log(this.secretSantaList);
-    };
 
+// volvemos a hacer suffle a los dos arrays
+      this.friendsList = this.friendsList.slice().sort(function(){
+        return Math.random()-0.5
+      });
+
+      this.suffle_list = this.suffle_list.slice().sort(function(){
+        return Math.random()-0.5
+      });
+
+    }
+})}
+
+      console.log("new friend list",this.newFriendlist);
+      console.log("new sufflelist",this.newSuffleList);
+
+      console.log("repetido friend list",this.friendsList);
+      console.log("repetido suffle",this.suffle_list);
+    };
 
   ngOnInit(): //objeto.buildSecretSanta();
   void {}
